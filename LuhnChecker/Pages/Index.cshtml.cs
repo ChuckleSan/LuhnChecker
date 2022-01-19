07589c83f7b173c6ce56a0eb391b71bdc6e91aed
Chris Huckle <chris@triangulum.co.za>
2022-01-19 11:58:16 +0200

    Add project files.

---
+++ LuhnChecker/Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using Services;
+
+namespace LuhnChecker.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILuhnCheckService _luhnCheck;
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(
+            ILuhnCheckService luhnCheck,
+            ILogger<IndexModel> logger)
+        {
+            _luhnCheck = luhnCheck;
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ LuhnChecker/Services/LuhnCheckService.cs
@@ -0,0 +1,21 @@
+﻿namespace Services
+{
+    public class LuhnCheckService : ILuhnCheckService
+
+    {
+        public bool LuhnCheck(string cardNumber)
+        {
+            return LuhnCheck(cardNumber.Select(c => c - '0').ToArray());
+        }
+
+        private bool LuhnCheck(int[] digits)
+        {
+            return GetCheckValue(digits) == 0;
+        }
+
+        private int GetCheckValue(int[] digits)
+        {
+            return digits.Select((d, i) => i % 2 == digits.Length % 2 ? ((2 * d) % 10) + d / 5 : d).Sum() % 10;
+        }
+    }
+}
